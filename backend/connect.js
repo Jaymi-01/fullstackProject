285e902c78f636355c3f1b8b0ae853b0942d0119
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-06-12 19:08:26 +0100

    called the database from mongo

--- backend/connect.js
+++ backend/connect.js
@@ -10,16 +10,24 @@ const client = new MongoClient(process.env.ATLAS_URI, {
   }
 });
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+let database
+
+module.exports ={
+    connectToServer: () => {
+        database = client.db("blogData")
+    }
 }
-run().catch(console.dir);
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
