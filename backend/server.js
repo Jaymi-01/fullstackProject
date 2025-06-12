9cb3e3b3fba42392191ad219651da8079c65a5cf
Joel Miller <99599435+Jaymi-01@users.noreply.github.com>
2025-06-12 19:28:01 +0100

    some yadas

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,9 @@
 const connect = require("./connect")
 const express = require("express")
 const cors = require("cors")
+
+const app = express()
+const PORT = 3000
+
+app.use(cors())
+app.use(express.json())
